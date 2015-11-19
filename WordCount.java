3f47e3ad186e39e768b974bf053c6a0c50b83cec
lyaronskaya <luba.yaronskaya@gmail.com>
2015-11-19 18:27:58 +0300

    task2

--- WordCount.java
+++ WordCount.java
@@ -37,20 +37,21 @@ public class WordCount {
             }
 
         };
+    public static void main(String[] args) throws Exception {
+        Configuration conf = new Configuration();
+        Job job = new Job(conf, "wordcount");
+        job.setJarByClass(WordCount.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+        job.setMapperClass(Map.class);
+        job.setReducerClass(Reduce.class);
+        job.setInputFormatClass(TextInputFormat.class);
+        job.setOutputFormatClass(TextOutputFormat.class);
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+        job.waitForCompletion(true);
+    }
+
 };
 
-public static void main(String[] args) throws Exception {
-    Configuration conf = new Configuration();
-    Job job = new Job(conf, "wordcount");
-    job.setJarByClass(WordCount.class);
-    job.setOutputKeyClass(Text.class);
-    job.setOutputValueClass(IntWritable.class);
-    job.setMapperClass(Map.class);
-    job.setReducerClass(Reduce.class);
-    job.setInputFormatClass(TextInputFormat.class);
-    job.setOutputFormatClass(TextOutputFormat.class);
-    FileInputFormat.addInputPath(job, new Path(args[0]));
-    FileOutputFormat.setOutputPath(job, new Path(args[1]));
-    job.waitForCompletion(true);
-}
 
